c8e614fb4b83f58082087fb613f4f81e8479d48e
shuangdai <dais@kbfoo.com>
2016-06-06 14:24:37 +0800

    first commit

---
+++ src/main/java/com/ulmm.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) 2016, kbfoo. All rights reserved.
+ */
+package com;
+
+/**
+ *
+ * @author <a href="mailto:2881090362@qq.com">戴爽</a>
+ * @version 1.0 16/6/6
+ * @since 1.0
+ */
+public class ulmm {
+
+    public static void main(String[] args) {
+
+    }
+
+}
